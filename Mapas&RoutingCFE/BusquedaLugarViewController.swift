ca8567714710fe275bc50285be9de0d4b467b13f
Marco Alonso <marco@garagecoders.net>
2022-08-10 13:08:53 -0500

    Ubicacion y coordenadas terminadas

--- Mapas&RoutingCFE/BusquedaLugarViewController.swift
+++ Mapas&RoutingCFE/BusquedaLugarViewController.swift
@@ -6,24 +6,60 @@
 //
 
 import UIKit
+import MapKit
 
 class BusquedaLugarViewController: UIViewController {
+    
+    @IBOutlet weak var mapa: MKMapView!
+    @IBOutlet weak var busquedaLugarSB: UISearchBar!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        busquedaLugarSB.delegate = self
+        
+        //Cual sera el primer obj en responder a la interaccion del usuario
+        busquedaLugarSB.becomeFirstResponder()
     }
+
+}
+
+// MARK: - SearchBar
+extension BusquedaLugarViewController: UISearchBarDelegate {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        //ocultar el teclado
+        busquedaLugarSB.resignFirstResponder()
+        
+        let geocoder = CLGeocoder() //para convertir entre lugar y coordenadas
+        
+        //crear un lugar para buscar que sea el texto que el usuario escribe
+        if busquedaLugarSB.text != "" {
+            if let direccion = busquedaLugarSB.text {
+                geocoder.geocodeAddressString(direccion) { (lugares: [CLPlacemark]?, error: Error?) in
+                    print("Lugares encontrados: \(lugares?.count)")
+                    //validar si hubo errro
+                    if error != nil {
+                        print(error!.localizedDescription)
+                        
+                        let alerta = UIAlertController(title: "Lugar o direccion no encontrado", message: "", preferredStyle: .alert)
+                        let accionAceptar = UIAlertAction(title: "Aceptar", style: .default, handler: nil)
+                       
+                        alerta.addAction(accionAceptar)
+                        
+                        self.present(alerta, animated: true, completion: nil)
+                    }// if error
+                    
+                    //Crear el lugar a mostrar
+                    if let lugar = lugares?.first {
+                        
+                    }
+                    
+                    
+                }//closure geocoder
+            }//if let direccion
+        }
+        
     }
-    */
-
 }
--- Mapas&RoutingCFE/UbicacionViewController.swift
+++ Mapas&RoutingCFE/UbicacionViewController.swift
@@ -6,15 +6,90 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class UbicacionViewController: UIViewController {
+    
+    @IBOutlet weak var Mapa: MKMapView!
+    
+    var manager = CLLocationManager()
+    
+    var latitud: CLLocationDegrees?
+    var longitud: CLLocationDegrees?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        manager.delegate = self
         
+        manager.requestWhenInUseAuthorization()
     }
+    
+    @IBAction func ubicacionBtn(_ sender: UIBarButtonItem) {
+        manager.requestLocation() //vuelve a pedir la ubicacion del usuario
+        
+        let ubicacionMapa = CLLocationCoordinate2D(latitude: latitud ?? 19.39484, longitude: longitud ?? -101.1874)
+        //nivel del zoom
+        let spanMapa = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let regionMapa = MKCoordinateRegion(center: ubicacionMapa, span: spanMapa)
+        
+        //Agregar la region al mapa y mostrar la ubicacion del usuario
+        Mapa.setRegion(regionMapa, animated: true)
+        Mapa.showsUserLocation = true
+    }
+    
+    @IBAction func CoordenadasBtn(_ sender: UIBarButtonItem) {
+        //Mostrar una alerta con las coordenadas del usuario
+        let alerta = UIAlertController(title: "Ubicacion", message: "Tu coordenadas son: Lat: \(latitud ?? 0) Lon: \(longitud ?? 0)", preferredStyle: .alert)
+        let accionAceptar = UIAlertAction(title: "Ok", style: .default) { (_) in
+        }
+        alerta.addAction(accionAceptar)
+        present(alerta, animated: true)
 
+    }
+    
+}
 
+extension UbicacionViewController: CLLocationManagerDelegate {
+    
+    //que hacer cuando se obtuvo la ubicacio didUpdateLocations
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("Se obtuvo la ubicacion")
+        if let ubicacion = locations.first {
+            latitud = ubicacion.coordinate.latitude
+            longitud = ubicacion.coordinate.longitude
+            //Dejar de obtener la ubicacion
+           // manager.stopUpdatingLocation()
+        }
+    }
+    
+    //que hacer cuando NO se obtuvo la ubicacio didFailWithError
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error al obtener ubicacion")
+    }
+    
+    
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            print("notDetermined")
+        case .restricted:
+            print("restricted")
+        case .denied:
+            print("denied")
+        case .authorizedAlways:
+            print("authorizedAlways")
+        case .authorizedWhenInUse:
+            print("authorizedWhenInUse")
+            //Acceder a la ubicacion
+            manager.requestLocation()
+        case .authorized:
+            print("authorized")
+        @unknown default:
+            fatalError("Error desconocido :/")
+        }
+    }
 }
 
